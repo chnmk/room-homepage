71b0e10c6e80e0d338ca9950eeec237b5d9d2ac8
chnmk <a2molchanov@ya.ru>
2023-12-10 16:20:46 +0300

    build: add basic webpack configuration

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { calc } from "./test.js"
+import './components/app.scss'
+
+console.log("Hello World!")
+console.log(calc(5, 5))
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+import { buildWebpack } from './config/build/buildWebpack.js';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default (env) => {
+    const paths = {
+        output: path.resolve(__dirname, 'build'),
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+        assetsFrom: path.resolve(__dirname, 'src/assets'),
+        assetsTo: path.resolve(__dirname, 'build/src/assets'),
+    }
+    const config = buildWebpack({
+        port: env.port ?? 3000,
+        mode: env.mode ?? 'development',
+        paths
+    })
+    return config
+} 
