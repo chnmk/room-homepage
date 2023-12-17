cd2bf1b29fd37549ef4227ec2e16f472b39142d8
chnmk <a2molchanov@ya.ru>
2023-12-17 13:01:04 +0300

    style: modify styles for mobile header menu

--- src/index.js
+++ src/index.js
@@ -8,9 +8,16 @@ const firstPageDescription = "We provide unmatched quality, comfort, and style f
 const secondPageDescription = "With stores all over the world, it's easy for you to find furniture for your home or place of business.\nLocally, we’re in most major cities throughout the country. Find the branch nearest you using our\nstore locator. Any questions? Don't hesitate to contact us today."
 const thirdPageDescription = "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology\nto ensure that every product is made as perfect and as consistent as possible. With three decades of\nexperience in this industry, we understand what customers want for their home and office."
 
-const imageElement = document.getElementsByClassName("main-image")[0];
-const openHamburgerElement = document.getElementsByClassName("nav-menu__mobile-menu")[0];
-const closeHamburgerElement = document.getElementsByClassName("nav-menu__mobile-menu-close")[0];
+const imageElement = document.getElementsByClassName("main-image")[0]
+const openHamburgerElement = document.getElementsByClassName("nav-menu__mobile-menu")[0]
+const closeHamburgerElement = document.getElementsByClassName("nav-menu__mobile-menu-close")[0]
+const logoElement = document.getElementsByClassName("nav-menu__logo")[0]
+const navBackgroundElement = document.getElementsByClassName("nav-menu")[0]
+const navElement = document.getElementsByClassName("page-nav__link-list")[0]
+const linkElement1 = document.getElementsByClassName("page-nav__link-1")[0]
+const linkElement2 = document.getElementsByClassName("page-nav__link-2")[0]
+const linkElement3 = document.getElementsByClassName("page-nav__link-3")[0]
+const linkElement4 = document.getElementsByClassName("page-nav__link-4")[0]
 
 let currentPage = 1
 
@@ -67,9 +74,23 @@ window.changePrevious = function() {
 window.openHamburger = function() {
     openHamburgerElement.classList.add("nav-menu__mobile-menu--disabled")
     closeHamburgerElement.classList.remove("nav-menu__mobile-menu-close--disabled")
+    logoElement.classList.add("nav-menu__logo--disabled")
+    navElement.classList.add("page-nav__link-list--mobile-enabled")
+    linkElement1.classList.add("page-nav__link-1--mobile-enabled")
+    linkElement2.classList.add("page-nav__link-2--mobile-enabled")
+    linkElement3.classList.add("page-nav__link-3--mobile-enabled")
+    linkElement4.classList.add("page-nav__link-4--mobile-enabled")
+    navBackgroundElement.classList.add("nav-menu-mobile-enabled")
 }
 
 window.closeHamburger = function() {
-    closeHamburgerElement.classList.add("nav-menu__mobile-menu-close--disabled")
     openHamburgerElement.classList.remove("nav-menu__mobile-menu--disabled")
+    closeHamburgerElement.classList.add("nav-menu__mobile-menu-close--disabled")
+    logoElement.classList.remove("nav-menu__logo--disabled")
+    navElement.classList.remove("page-nav__link-list--mobile-enabled")
+    linkElement1.classList.remove("page-nav__link-1--mobile-enabled")
+    linkElement2.classList.remove("page-nav__link-2--mobile-enabled")
+    linkElement3.classList.remove("page-nav__link-3--mobile-enabled")
+    linkElement4.classList.remove("page-nav__link-4--mobile-enabled")
+    navBackgroundElement.classList.remove("nav-menu-mobile-enabled")
 }
